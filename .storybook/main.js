6d2bbfbe8962c7196d7e542857454041889bfb3f
kotomi1338 <kotomin1216@gmail.com>
2022-02-15 19:55:09 +0900

    done: 単純なコンポーネントを作る

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  //👇 Location of our stories
+  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  core: {
+    builder: 'webpack5',
+  },
+};
